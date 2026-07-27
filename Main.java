12909c88540c77cca3a65eb346463eabcf081d2e
ChaitaliMahajan08 <chaitalimahajan08@gmail.com>
2026-07-27 14:09:41 +0530

    Uncomment code and fix formatting in Main.java

--- Main.java
+++ Main.java
@@ -1,140 +1,140 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// public class Main {
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner();
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner();
 
-//         int N = sc.nextInt();
+        int N = sc.nextInt();
 
-//         for (int i = 1; i <= N; i++) {
-//             if (i % 2 == 0) {
-//                 System.out.println(i + " Even");
-//             } else {
-//                 System.out.println(i + " Odd");
-//             }
-//         }
+        for (int i = 1; i <= N; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " Even");
+            } else {
+                System.out.println(i + " Odd");
+            }
+        }
 
-//         sc.close();
-//     }
-// }
+        sc.close();
+    }
+}
 
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// public class Main {
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-//         int N = sc.nextInt();
+        int N = sc.nextInt();
 
-//         for (int i = 1; i <= N; i++) {
-//             System.out.println("Table of " + i);
+        for (int i = 1; i <= N; i++) {
+            System.out.println("Table of " + i);
 
-//             for (int j = 1; j <= 10; j++) {
-//                 System.out.println(i + " x " + j + " = " + (i * j));
-//             }
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + " x " + j + " = " + (i * j));
+            }
 
-//             System.out.println(); 
-//         }
+            System.out.println(); 
+        }
 
-//         sc.close();
-//     }
-// }
+        sc.close();
+    }
+}
 
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// public class Main {
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-//         int start = sc.nextInt();
-//         int end = sc.nextInt();
+        int start = sc.nextInt();
+        int end = sc.nextInt();
 
-//         for (int i = start; i <= end; i++) {
-//             if (i < 2) {
-//                 continue;
-//             }
+        for (int i = start; i <= end; i++) {
+            if (i < 2) {
+                continue;
+            }
 
-//             boolean isPrime = true;
+            boolean isPrime = true;
 
-//             for (int j = 2; j <= i / 2; j++) {
-//                 if (i % j == 0) {
-//                     isPrime = false;
-//                     break;
-//                 }
-//             }
+            for (int j = 2; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
 
-//             if (isPrime) {
-//                 System.out.println(i);
-//             }
-//         }
+            if (isPrime) {
+                System.out.println(i);
+            }
+        }
 
-//         sc.close();
-//     }
-// }
+        sc.close();
+    }
+}
 
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// public class Main {
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-//         int secret = 27;
-//         boolean guessed = false;
+        int secret = 27;
+        boolean guessed = false;
 
-//         for (int i = 1; i <= 5; i++) {
-//             int guess = sc.nextInt();
+        for (int i = 1; i <= 5; i++) {
+            int guess = sc.nextInt();
 
-//             if (guess < secret) {
-//                 System.out.println("Too Low");
-//             } 
-//             else if (guess > secret) {
-//                 System.out.println("Too High");
-//             } 
-//             else {
-//                 System.out.println("Congratulations");
-//                 guessed = true;
-//                 break;
-//             }
-//         }
+            if (guess < secret) {
+                System.out.println("Too Low");
+            } 
+            else if (guess > secret) {
+                System.out.println("Too High");
+            } 
+            else {
+                System.out.println("Congratulations");
+                guessed = true;
+                break;
+            }
+        }
 
-//         if (!guessed) {
-//             System.out.println("Better Luck Next Time!");
-//         }
+        if (!guessed) {
+            System.out.println("Better Luck Next Time!");
+        }
 
-//         sc.close();
-//     }
-// }
+        sc.close();
+    }
+}
 
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// public class Main {
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-//         int start = sc.nextInt();
-//         int end = sc.nextInt();
+        int start = sc.nextInt();
+        int end = sc.nextInt();
 
-//         for (int i = start; i <= end; i++) {
-//             int num = i;
-//             int sum = 0;
+        for (int i = start; i <= end; i++) {
+            int num = i;
+            int sum = 0;
 
-//             while (num > 0) {
-//                 int digit = num % 10;
+            while (num > 0) {
+                int digit = num % 10;
 
-//                 int fact = 1;
-//                 for (int j = 1; j <= digit; j++) {
-//                     fact = fact * j;
-//                 }
+                int fact = 1;
+                for (int j = 1; j <= digit; j++) {
+                    fact = fact * j;
+                }
 
-//                 sum = sum + fact;
-//                 num = num / 10;
-//             }
+                sum = sum + fact;
+                num = num / 10;
+            }
 
-//             if (sum == i) {
-//                 System.out.println(i);
-//             }
-//         }
+            if (sum == i) {
+                System.out.println(i);
+            }
+        }
 
-//         sc.close();
-//     }
-// }
+        sc.close();
+    }
+}
